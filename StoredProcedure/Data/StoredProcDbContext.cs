65d4c834028311ab1846c8583c6caa6b3b97a121
annemariv <annemari.velling@gmail.com>
2025-06-04 11:02:36 +0300

    Add project

---
+++ StoredProcedure/Data/StoredProcDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StoredProcedure.Models;
+
+namespace StoredProcedure.Data
+{
+    public class StoredProcDbContext : DbContext
+    {
+        public StoredProcDbContext(DbContextOptions<StoredProcDbContext>options): base(options) { }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
